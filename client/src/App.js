584cac62c42d2cfcbd8043d49b1268b63dab04fa
Tim Petrvalsky <tpet55@yahoo.com>
2021-04-16 10:24:56 -0700

    Not working

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,63 @@ function App() {
 		setState({ page: 1, category: 67 });
 	}, []);
 
+	// 	const getApiData = async (genreId, page) => {
+	// 		let t0 = performance.now();
+	// 		await fetch(
+	// 			`https://listen-api.listennotes.com/api/v2/best_podcasts?genre_id=${genreId}&page=${page}&region=us&safe_mode=0`,
+	// 			{
+	// 				method: 'GET',
+	// 				headers: {
+	// 					'Content-Type': 'application/json',
+	// 					'X-ListenAPI-Key': process.env.REACT_APP_LISTEN_NOTES_API_KEY
+	// 				},
+	// 				credentials: 'same-origin'
+	// 			}
+	// 		).then((response) => {
+	// 			response.json().then((data) => {
+	// 				console.log(data, 'data in listennotes call see if doubling');
+	// 				const getRating = async () => {
+	// 					await axios({
+	// 						method: 'post',
+	// 						url: `http://localhost:7000/findId`,
+	// 						headers: {},
+	// 						data: {
+	// 							pod: data.podcasts
+	// 						}
+	// 					})
+	// 						.then(function(response) {
+	// 							console.log(response.data);
+	// 						})
+	// 						.catch(function(error) {
+	// 							console.log(error);
+	// 						});
+
+	// 					// for (let pod of data.podcasts) {
+	// 					// 	const id = pod.id;
+	// 					// 	await axios
+	// 					// 		.get(`http://localhost:7000/findId/?data=${id}`)
+	// 					// 		.then(function(response) {
+	// 					// 			pod['rating'] = response.data.rating;
+	// 					// 			pod['numberOfRatings'] = response.data.numberOfRatings || 'N/A';
+	// 					// 			pod['itunes'] = response.data.itunes;
+	// 					// 		})
+	// 					// 		.catch(function(error) {
+	// 					// 			console.log(error);
+	// 					// 		});
+	// 					// }
+	// 					await setPodcasts([ data.podcasts ]);
+	// 				};
+	// 				getRating();
+	// 			});
+	// 		});
+	// 		let t1 = performance.now();
+	// 		console.log('Call to execute main function took' + (t1 - t0) + 'milliseconds');
+	// 	};
+
+	// 	return <Header podcasts={podcasts} getApiData={getApiData} />;
+	// }
 	const getApiData = async (genreId, page) => {
+		let t0 = performance.now();
 		await fetch(
 			`https://listen-api.listennotes.com/api/v2/best_podcasts?genre_id=${genreId}&page=${page}&region=us&safe_mode=0`,
 			{
@@ -28,24 +84,28 @@ function App() {
 			response.json().then((data) => {
 				console.log(data, 'data in listennotes call see if doubling');
 				const getRating = async () => {
-					for (let pod of data.podcasts) {
-						const id = pod.id;
-						await axios
-							.get(`http://localhost:7000/findId/?data=${id}`)
-							.then(function(response) {
-								pod['rating'] = response.data.rating;
-								pod['numberOfRatings'] = response.data.numberOfRatings || 'N/A';
-								pod['itunes'] = response.data.itunes;
-							})
-							.catch(function(error) {
-								console.log(error);
-							});
-					}
-					await setPodcasts([ data.podcasts ]);
+					await axios({
+						method: 'post',
+						url: `http://localhost:7000/findId/`,
+						data: {
+							pod: data.podcasts
+						}
+					})
+						.then(function(response) {
+							console.log(response, 'response from backend with fullarray');
+						})
+						.catch(function(error) {
+							console.log(error);
+						});
+
+					await setPodcasts([ response.data ]);
 				};
 				getRating();
 			});
 		});
+		console.log(podcasts, 'podcasts after full backend run');
+		let t1 = performance.now();
+		console.log('Call to execute main function took' + (t1 - t0) + 'milliseconds');
 	};
 
 	return <Header podcasts={podcasts} getApiData={getApiData} />;
