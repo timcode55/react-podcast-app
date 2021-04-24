e25b77e9c0050f3d55339e292cd85c5bb8f147dd
Tim Petrvalsky <tpet55@yahoo.com>
2021-04-24 12:57:49 -0700

    Add docker functionality

--- server/app.js
+++ server/app.js
@@ -6,11 +6,14 @@ require('./db/mongoose');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 7000;
 // const bodyParser = require('body-parser');
 
+app.use(morgan('combined'));
 app.use(cors());
 app.use(express.json());
+console.log('awesome');
 // app.use(express.urlencoded());
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
