2b5d63feab077d71868d0cffbf4ffc88948a6143
Tim Petrvalsky <tpet55@yahoo.com>
2021-05-14 17:00:06 -0700

    Graphql in progress

--- server/app.js
+++ server/app.js
@@ -7,7 +7,50 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 7000;
+const { graphqlHTTP } = require('express-graphql');
 const bodyParser = require('body-parser');
+const { buildSchema } = require('graphql');
+const expressPlayground = require('graphql-playground-middleware-express').default;
+
+app.use(bodyParser.json());
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: buildSchema(`
+      type RootQuery {
+        hello: String!
+      }
+
+      type RootMutation{
+        addUser(userInput: UserInput!):User!
+      }
+
+      type User {
+        _id: ID!
+        email: String!
+        password: String!
+      }
+
+      input UserInput {
+        email: String!
+        password: String!
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+		rootValue: {
+			hello: () => {
+				return 'Hello Back!!';
+			}
+		},
+		graphiql: true
+	})
+);
 
 app.use(cors());
 app.use((req, res, next) => {
