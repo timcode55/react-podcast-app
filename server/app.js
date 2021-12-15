ed27231fa5d4b685ffafd4d4b50daf9a53a5552f
Tim Petrvalsky <tpet55@yahoo.com>
2021-12-15 16:39:09 -0700

    Clean up code

--- server/app.js
+++ server/app.js
@@ -1,13 +1,8 @@
-const mongoose = require("mongoose");
-const fs = require("fs");
-const cheerio = require("cheerio");
-const request = require("request-promise");
 require("./db/mongoose");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 7000;
-const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use((req, res, next) => {
@@ -29,7 +24,6 @@ app.get("/test", (req, res) => res.send("Test Route"));
 
 app.get("/findId", async (req, res, next) => {
   let id = req.query.data;
-  // console.log(id, 'id');
   try {
     const podcast = await Rating.findOne({ id }).lean();
     res.send(podcast);
@@ -39,7 +33,7 @@ app.get("/findId", async (req, res, next) => {
 });
 
 // app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
-console.log(process.env.PORT);
+// console.log(process.env.PORT);
 app.listen(process.env.PORT || 7000, () =>
   console.log(`App listening on port ${PORT}`)
 );
